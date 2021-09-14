cff4bbd06de5ea12386f168ae4766051e6c9dcc0
Opilane TTHK <opilane@tthk.ee>
2021-09-14 12:06:44 +0300

    add OR and AND

---
+++ BooleanLogicalOperators/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BooleanLogicalOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada kasutajatunnus
+            //programm küsib kasutajal sisestada parool
+            //programm kontrollib, kas mõlemad, nii kasutajatunnus kui ka parool on õiged
+            //kui on õiged, siis programm kuvab konsoolis "tere tulemast!"
+            //kui on valed, siis programm kuvab sisselogimine ebaõnnestus.Prooviuuesti."
+            //kt: admin, pr: admin1234
+
+            Console.WriteLine("Sisestage kasutajatunnus:");
+            string UserName = Console.ReadLine();
+            Console.WriteLine("Sisestage parool:");
+            string Password = Console.ReadLine();
+
+            if (UserName == "admin" && Password == "admin1234")
+            {
+                Console.WriteLine("Tere tulemast!");
+            }
+            else
+            {
+                Console.WriteLine("Sisselogimine ebaõnnestus. Proovi uuesti.");
+            }
+
+
+            
+
+
+        }
+    }
+}
---
+++ LogIn/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LogIn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisestage kasutajatunnus:");
+            string UserName = Console.ReadLine();
+            Console.WriteLine("Sisestage parool:");
+            string Password = Console.ReadLine();
+
+            if (UserName != "admin" || Password != "admin1234")
+            {
+                Console.WriteLine("Sisselogimine ebaõnnestus. Proovi uuesti.");
+            }
+            else
+            {
+                Console.WriteLine("Tere tulemast!");
+            }
+
+
+
+
+
+        }
+    }
+}
